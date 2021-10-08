253abaa666d0f72acf4be03202d24c5e32d341a1
Glitch (doggie-tinder) <none>
2021-10-08 14:36:09 +0000

    📌☄️ Updated with Glitch

--- script.js
+++ script.js
@@ -1,29 +1,71 @@
 /* Global Vue */
 
+
+if (location.protocol == 'http:') {
+  location.protocol = 'https:';
+}
+
 const app = new Vue({
   el: "#app",
   data: {
     currentDogUrl: null,
-    favorites: []
+    favorites: [],
+    online: true,
   },
+  
   methods: {
-    loadDog: async function() {
+    loadDog: async function () {
       const response = await fetch("https://dog.ceo/api/breeds/image/random");
       const asJson = await response.json();
       this.currentDogUrl = asJson.message;
       console.log(this.currentDogUrl);
     },
-    addFav: function() {
+    
+    addFav: async function() {
       if (!this.favorites.includes(this.currentDogUrl)) {
         this.favorites.push(this.currentDogUrl);
+
+        const imageCache = await caches.open("favoriteImageCache");
+        imageCache.add(this.currentDogUrl);
       }
       this.loadDog();
     },
+    
     removeFav: function(dog) {
       this.favorites = this.favorites.filter(item => item !== dog);
     }
   },
+  
   created() {
     this.loadDog();
+  },
+  
+  mounted() {
+    if (localStorage.favorites) {
+      this.favorites = JSON.parse(localStorage.favorites);
+    }
+    
+    const onlineStatus = navigator.onLine;
+    addEventListener("offline", () => {
+      this.online = false;
+    });
+    addEventListener("online", () => {
+      this.online = true;
+    });
+  },
+  
+  watch: {
+    favorites(updatedfavorites) {
+      localStorage.favorites = JSON.stringify(updatedfavorites);
+    },
+    online(isOnline) {
+      if(isOnline) {
+        this.loadDog();
+      }    
+    }
   }
 });
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js");
+}
